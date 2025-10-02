9fbf1694b2846afee974bfc1f8cf9f7aa6e0fd30
hyerin2411931 <2411931@sookmyung.ac.kr>
2025-10-02 15:31:14 +0900

    Week05_04

--- main.c
+++ main.c
@@ -4,13 +4,15 @@
 int main(int argc, char *argv[]){
      
   int x;
-  printf("Input an integer: ");
+  int i;   //for문 돌리기 위한 변수 
+  int sum = 0;
+  printf("Input an number: ");
   scanf("%d", &x);
   
-  if(x > 0)
-      printf("absolute is %i.\n", x); 
-  else if(x < 0)
-       printf("absolute is %i\n", -x);
+  for(i= 1; i < x+1 ; i ++)
+        sum = sum + i;     //for문 안에는 i가 들어가야 함. x 말고.. => x 들어가면 걍 입력받은 x를 x번 더하게 되는 식이 됨. 
+        
+  printf("The result is %i.\n", sum); 
        
   system("PAUSE");	
   return 0;
